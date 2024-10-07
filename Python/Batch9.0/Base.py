d04e96bf0cff141252150d85b7c4b1036478da2c
Chanakya <Chanakyamanas123@gmail.com>
2024-10-07 10:54:14 +0530

    test

---
+++ Python/Batch9.0/Base.py
@@ -0,0 +1,19 @@
+a=56
+print(a)
+print(type(a))
+p=float(a)
+print(p)
+a="Hello"
+print(a)
+
+a=b=c="hello world"
+print(a)
+a="Hi"
+b="Hello"
+c="Bye"
+d=23
+a,b,c="Hi","Hello","Bye"
+print(b)
+pov=1j
+print(type(pov))
+print(b,d)
